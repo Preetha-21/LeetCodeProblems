938c65980d5c7f59abdefe71d9f11acda90ff21a
Preetha-21 <preethakaliappan289@gmail.com>
2026-07-31 14:06:54 +0530

    Time: 172 ms (5.41%) | Memory: 46.9 MB (5.80%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public boolean isValid(String s)
+    {
+       while (s.contains("()") || s.contains("[]") || s.contains("{}"))
+       {
+        if(s.contains("()"))
+        {
+            s=s.replace("()","");
+        }
+        else if(s.contains("[]"))
+        {
+            s=s.replace("[]","");
+        }
+        else if(s.contains("{}"))
+        {
+            s=s.replace("{}","");
+        }
+       }
+      
+       return s.isEmpty();
+    }
+}
