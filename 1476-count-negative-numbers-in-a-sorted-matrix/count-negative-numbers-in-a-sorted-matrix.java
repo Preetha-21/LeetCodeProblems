8cff0b009c273c3fde78f17e96e229adc2d74bb2
Preetha-21 <preethakaliappan289@gmail.com>
2026-07-31 19:35:15 +0530

    Time: 1 ms (56.36%) | Memory: 46.8 MB (66.43%) - LeetSync

---
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count=0;
+        for(int row[]:grid)
+        {
+            for(int elements:row)
+            {
+                if(elements<0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
