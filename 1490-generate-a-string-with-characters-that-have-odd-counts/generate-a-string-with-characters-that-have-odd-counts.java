858afb648ba322e91f171b24270a6505f0fb05c0
Preetha-21 <preethakaliappan289@gmail.com>
2026-07-31 14:25:25 +0530

    Time: 4 ms (18.47%) | Memory: 46.2 MB (12.18%) - LeetSync

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String generateTheString(int n) {
+        String result="";
+        if(n%2==1)
+        {
+            for(int i=0;i<n;i++)
+            {
+                result+='a';
+            }
+        }
+        else{
+
+            for(int i=0;i<n-1;i++)
+            {
+                result+='a';
+            }
+            result+='b';
+        }
+        return result;
+    }
+}
