e9693915e717b1621448ca5c64244db2aa2b9e52
Preetha-21 <preethakaliappan289@gmail.com>
2026-07-31 20:49:36 +0530

    Update majority-element.java

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);        // sort the array
-        return nums[nums.length / 2];  // middle element is majority
+        Arrays.sort(nums);       
+        return nums[nums.length / 2];  
     }
 }
