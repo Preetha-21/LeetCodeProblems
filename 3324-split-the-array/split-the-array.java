e676c6deee2d61bd3f739e5556e081e8202cfcff
Preetha-21 <preethakaliappan289@gmail.com>
2026-08-06 14:34:53 +0530

    Time: 4 ms (8.37%) | Memory: 46.3 MB (7.84%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        int arr[]=new int[1000];
+        for(int i=0;i<nums.length;i++)
+        {
+            arr[nums[i]]++;
+        }
+        int count=0;
+        for(int i:arr)
+        {
+            if(i==2||i==0)
+            {
+                continue;
+            }
+            else if(i==1)
+            {
+                count++;
+            }
+            else{
+                return false;
+            }
+        }
+        return count%2==0;
+    }
+}
